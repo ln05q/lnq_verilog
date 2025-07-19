d76e0ea3223794754d5d99d3f7925b7cc198b81d
ln05q <lenhatquang11a1@gmail.com>
2025-07-20 04:10:01 +0700

    update register_file.v

--- register_file.v
+++ register_file.v
@@ -1,15 +1,15 @@
 module register_file (
     input clk,
-    input [4:0]ReadAddress1,
-    input [4:0]ReadAddress2,
-    input [4:0]WriteAddress,
+    input [4:0] ReadAddress1,
+    input [4:0] ReadAddress2,
+    input [4:0] WriteAddress,
     input [31:0] WriteData,
     input ReadWriteEn,
-    output [31:0] ReadData1,
-    output [31:0] ReadData2
+    output reg [31:0] ReadData1,
+    output reg [31:0] ReadData2
 );
 
-  reg [31:0] register[31];
+  reg [31:0] register[31:0];
 
   always @(posedge clk) begin
     if (ReadWriteEn) begin
@@ -17,4 +17,13 @@ module register_file (
     end
   end
 
+  always @(*) begin
+    ReadData1 = register[ReadAddress1];
+    ReadData2 = register[ReadAddress2];
+  end
+
+  initial begin
+    register[2] = 32'd13;
+    register[3] = 32'd28;
+  end
 endmodule
